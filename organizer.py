7df6447c71e00a47cf821af51672cab0ff124057
Arashomranpour <87114116+Arashomranpour@users.noreply.github.com>
2023-06-07 14:26:45 +0330

    Add files via upload

---
+++ organizer.py
@@ -0,0 +1,26 @@
+# import shutil
+# import os
+
+# target_folder = "path"
+# extensions = {
+#     item.split(".")[-1]
+#     for item in os.listdir(target_folder)
+#     if os.path.isfile(os.path.join(target_folder, item))
+# }
+# # print(extensions)
+
+# for exten in extensions:
+#     if not os.path.exists(os.path.join(target_folder, exten)):
+#         os.mkdir(os.path.join(target_folder, exten))
+
+
+# for item in os.listdir(target_folder):
+#     if os.path.isfile(os.path.join(target_folder, item)):
+#         file_exten = item.split(".")[-1]
+#         shutil.move(
+#             os.path.join(target_folder, item),
+#             os.path.join(target_folder, file_exten, item),
+#         )
+from datetime import date
+date=date.today()
+date
